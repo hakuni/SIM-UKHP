720c30a30323b2db9daadc5543688ba303f358d8
hakuni <ilham.krn02@gmail.com>
2019-02-19 15:11:18 +0700

    another fixing

--- database/migrations/2019_02_19_114306_create_vw_trx_penelitians.php
+++ database/migrations/2019_02_19_114306_create_vw_trx_penelitians.php
@@ -29,19 +29,24 @@ CREATE VIEW `vw_trxPenelitians` AS
 SELECT
     `p`.`idPenelitian` AS `idPenelitian`,
     `tp`.`idTrxPenelitian` AS `idTrxPenelitian`,
+    `mp`.`idProsedur` AS `idProsedur`,
+    `mp`.`judulPenelitian` AS `judulPenelitian`,
+    `mdc`.`namaPeneliti` AS `namaPeneliti`,
+    `mdc`.`instansiPeneliti` AS `instansiPeneliti`,
     `ltp`.`PIC` AS `PIC`,
     `mm`.`idMilestone` AS `idMilestone`,
     `mm`.`namaMilestone` AS `namaMilestone`,
     `vk`.`biaya` AS `biaya`,
     TO_DAYS(`tp`.`startDate`) - TO_DAYS(NOW()) AS `durasi`,
-    `mp`.`perlakuan` AS `perlakuan`,
-    `mp`.`idProsedur`
+    `mp`.`perlakuan` AS `perlakuan`
+    
     FROM
         `mst_penelitians` `p` LEFT JOIN `trx_penelitians` `tp` ON `p`.`idPenelitian` = `tp`.`idPenelitian`
          LEFT JOIN `mst_milestones` `mm` ON `p`.`lastMilestoneID` = `mm`.`idMilestone`
-         LEFT JOIN `sim-ukhp`.`log_trx_penelitians` `ltp` ON `p`.`idPenelitian` = `ltp`.`idPenelitian` 
-         LEFT JOIN `sim-ukhp`.`mst_prosedurs` `mp` ON `p`.`idPenelitian` = `mp`.`idPenelitian`
-         LEFT JOIN `sim-ukhp`.`vw_keuangans` `vk` ON `vk`.`idPenelitian` = `p`.`idPenelitian`
+         LEFT JOIN `log_trx_penelitians` `ltp` ON `p`.`idPenelitian` = `ltp`.`idPenelitian` 
+         LEFT JOIN `mst_prosedurs` `mp` ON `p`.`idPenelitian` = `mp`.`idPenelitian`
+         LEFT JOIN `vw_keuangans` `vk` ON `vk`.`idPenelitian` = `p`.`idPenelitian`
+         LEFT JOIN `mst_data_clients` `mdc` ON `p`.`idDataClient` = `mdc`.`idDataClient`
 SQL;
     }
 }
